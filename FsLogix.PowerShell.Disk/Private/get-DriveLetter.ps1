536439319613bb3dab21b74fd6724d67e8f523f5
Yong Kim <yhk18349@uga.edu>
2018-06-21 16:50:59 -0400

    Return Drive letter if already mounted
    If the VHD is already attached, script does not need to mount the VHD.
    Simply get the Drive letter by accessing it's partition.

--- FsLogix.PowerShell.Disk/Private/get-DriveLetter.ps1
+++ FsLogix.PowerShell.Disk/Private/get-DriveLetter.ps1
@@ -1,27 +1,27 @@
-<#
-    .NOTES
-    Created on 6/6/18
-    Created by Daniel Kim @ FSLogix    
-    Created by Jim Moyle @ FSLogix
-
-    .SYNOPSIS
-    Obtains a VHD path and mounts the VHD to the next available
-    drive Letter. Allows ability to search and use folders/items
-    within a VHD. User will need to then dismount VHD on their
-    own. 
-
-    .DESCRIPTION
-    This function can be added to any script that requires mounting 
-    a vhd and accessing it's contents.
-
-    .PARAMETER VHDPath
-    The target path for VHD location.
-
-    .EXAMPLE
-    mount-FSLVHD -path \\server\share\ODFC\vhd1.vhdx
-    Will mount vhd1.vhdx to next available drive letter.
-#>
 function get-driveletter {
+    <#
+        .NOTES
+        Created on 6/6/18
+        Created by Daniel Kim @ FSLogix    
+        Created by Jim Moyle @ FSLogix
+
+        .SYNOPSIS
+        Obtains a VHD path and mounts the VHD to the next available
+        drive Letter. Allows ability to search and use folders/items
+        within a VHD. User will need to then dismount VHD on their
+        own. 
+
+        .DESCRIPTION
+        This function can be added to any script that requires mounting 
+        a vhd and accessing it's contents.
+
+        .PARAMETER VHDPath
+        The target path for VHD location.
+
+        .EXAMPLE
+        mount-FSLVHD -path \\server\share\ODFC\vhd1.vhdx
+        Will return the drive letter
+    #>
 
     param(
         [Parameter(Position = 0, Mandatory = $true)]
@@ -30,10 +30,12 @@ function get-driveletter {
     )
     begin {
         Set-StrictMode -Version Latest
+    }
+    process {
 
-        $checkPath = Test-Path $VHDPath
-        
-        if ($checkPath) {
+        $Attached = $false
+
+        if (test-path $VHDPath) {
             Write-Verbose "$VHDPath is valid."
         }
         else {
@@ -42,47 +44,66 @@ function get-driveletter {
         }
 
         $VHDProperties = get-vhd -path $VHDPath
+
         if ($VHDProperties.Attached -eq $true) {
-            Write-Error "VHD is already attached/Mounted."
-            exit;
-        }
-    }
-    process {
-        
-        try {
-            $mount = Mount-VHD -path $VHDPath -Passthru -ErrorAction Stop
-            Write-Verbose "VHD succesfully mounted."
-        }
-        catch {
-            write-error $Error[0]
-            Write-Error "Could not mount VHD. Perhaps the VHD Path is incorrect or already attached."
-            break
+            $Attached = $true        
         }
 
-        #Obtain drive letter
-        $driveLetter = $mount | Get-Disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
+        if ($Attached) {
+
+            ## If disk is already mounted, can skip mounting process.
+            $disk = Get-Disk | Where-Object {$_.Location -eq $VHDPath}
+            $driveLetter = $disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
+
+        }
+        else {
+        
+            try {
+                ## Need to mount
+                $mount = Mount-VHD -path $VHDPath -Passthru -ErrorAction Stop
+                Write-Verbose "VHD succesfully mounted."
+            }
+            catch {
+                write-error $Error[0]
+                Write-Error "Could not mount VHD. Perhaps the VHD Path is incorrect or already attached."
+                break
+            }
+            #Obtain drive letter
+            $driveLetter = $mount | Get-Disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
+        }
     
-        if($null -eq $driveLetter){
+        if ($null -eq $driveLetter) {
 
             Write-warning "DriveLetter is null, assigning drive letter."
-            $mount | Get-Disk -Passthru |New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false -Force
-            $driveLetter = $mount | Get-Disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
-        
+            if ($Attached) {
+                $disk = Get-Disk | Where-Object {$_.Location -eq $VHDPath} | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false -Force
+                $driveLetter = $disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
+            }
+            else {
+                $mount | Get-Disk -Passthru |New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false -Force
+                $driveLetter = $mount | Get-Disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
+            }
         }
 
         #A drive letter was never initialized to the VHD
         if ($driveLetter -like "*\\?\Volume{*") {
 
             Write-warning "Driveletter is invalid: $Driveletter. Reassigning Drive Letter."
-            $driveLetter = $mount | get-disk | Get-Partition | Add-PartitionAccessPath -AssignDriveLetter | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
-                        
+            if ($Attached) {
+                $disk = Get-Disk | Where-Object {$_.Location -eq $VHDPath}
+                $driveLetter = $disk | Get-Partition | Add-PartitionAccessPath -AssignDriveLetter
+            }
+            else {
+                $driveLetter = $mount | get-disk | Get-Partition | Add-PartitionAccessPath -AssignDriveLetter 
+            }      
             <# For some reason, after assigning an partition access path drive letter, the variable
-               driveLetter will be null unless remounted. Maybe the code above (line 76) needs to be
+               driveLetter will be null unless remounted. Maybe the code above needs to be
                assigned differently  #>
             if ($null -eq $driveLetter) {
                 #Refresh mount
                             
                 try {
+                    Write-Verbose "Remounting VHD."
                     Dismount-VHD $VHDPath -Passthru -ErrorAction Stop
                 }
                 catch {
@@ -91,7 +112,7 @@ function get-driveletter {
                 }
 
                 try {
-                    Mount-VHD $VHDPath -Passthru -ErrorAction Stop
+                    $mount = mount-vhd -path $VHDPath -Passthru -ErrorAction stop
                     $driveLetter = $mount | get-disk | Get-Partition | Select-Object -ExpandProperty AccessPaths | Select-Object -first 1
                     Write-Verbose "Remounted VHD"
                 }
