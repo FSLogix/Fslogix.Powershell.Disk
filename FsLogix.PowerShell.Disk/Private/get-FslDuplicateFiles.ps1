39fcaaed1bbb0fd608abe8c59f68fc09bf31d5f5
Yong Kim <yhk18349@uga.edu>
2018-06-22 14:21:55 -0400

    Update Duplicates script

---
+++ FsLogix.PowerShell.Disk/Private/get-FslDuplicateFiles.ps1
@@ -0,0 +1,86 @@
+function get-FslDuplicateFiles {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+        [System.String]$path,
+
+        [Parameter(Position = 1, Mandatory = $false, ValueFromPipeline = $true)]
+        [System.String]$Folderpath,
+
+        [Parameter(Position = 2, Mandatory = $true, ValueFromPipeline = $true)]
+        [System.String]$Csvpath
+    )
+    
+    begin {
+        set-strictmode -Version latest
+    }
+    
+    process {
+        $HashArray = new-object System.Collections.ArrayList        
+        $name = split-path -path $path -leaf
+
+        $DriveLetter = get-Driveletter -path $path
+        $Path_To_Search = join-path ($DriveLetter)($folderpath)
+        Write-Verbose "Searching for Duplicates in $path"
+
+        if (-not(test-path -path $Path_To_Search)) {
+            Write-Warning "$name : Could not find path: $Path_To_Search"
+        }
+
+        ## Find Duplicate Algorithm ##
+        ## Is there a faster comparing algorithm? ##
+        $files = get-childitem -path $Path_To_Search -Recurse | Sort-Object -Property LastWriteTime -Descending
+        foreach ($file in $files) {
+
+            $currentfile = 0
+            try {
+                ## Get File's hash value, skipping if folder ##
+                $get_FileHash = Get-FileHash -path $file.fullname
+                $FileHash = $get_FileHash.hash
+                Write-Verbose "Obtained $file's hash value."
+            }
+            catch [System.Management.Automation.PropertyNotFoundException] {
+                Write-Warning "'$file' is not a file... Skipping."
+                continue
+            }
+
+            ## Compare Current hash to rest of childitem's hash values ##
+            foreach ($cmpFile in $files | where-object {$_.Name -ne $file.Name}) {
+                    
+                try {
+                    $get_FileHash = get-filehash -path $cmpFile.fullname
+                    $cmpHash = $get_FileHash.hash
+                }
+                catch [System.Management.Automation.PropertyNotFoundException] {
+                    Write-Warning "'$cmpfile' is not a file... Skipping."
+                    continue
+                }   
+
+                ## Avoid comparing duplicates we've already checked ##
+                if ($HashArray.Contains($cmpHash)) {
+                    Write-Verbose "Already found $cmpfile's duplicates"
+                    break;
+                }
+
+                if ($FileHash -eq $cmpHash) {
+                    Write-Verbose "Duplicate found!"
+                    if ($currentfile -eq 0) {
+                        $currentfile++
+                        $output = $name + ',' + $file.name + ',' + $cmpFile.fullname
+                    }
+                    else {
+                        $output = ',' + ',' + $cmpFile.FullName
+                    }
+                    Add-Content -path $Csvpath $output
+                }
+            }
+
+            ## We found all duplicates of this hash. No more comparisons ##
+            $HashArray.Add($FileHash) > $null
+        }#foreach
+        dismount-FslDisk -path $path
+    }#process
+    
+    end {
+    }
+}
--- FsLogix.PowerShell.Disk/Public/Get-FslDuplicates.ps1
+++ FsLogix.PowerShell.Disk/Public/Get-FslDuplicates.ps1
@@ -19,7 +19,7 @@ function Get-FslDuplicates {
         [System.String]$Path,
 
         [Parameter(Position = 2, Mandatory = $true)]
-        [System.String]$Csvpath,
+        [System.String]$Csvpath = '$env:temp\test.csv',
 
         [Parameter(Position = 3, Mandatory = $false, ValueFromPipeline = $true)]
         [Alias("Confirm")]
@@ -68,70 +68,9 @@ function Get-FslDuplicates {
         
         ## Search Duplicates ##
         foreach ($vhd in $VHDs) {
-
-            $HashArray = new-object System.Collections.ArrayList
-            $name = split-path -path $vhd.path -leaf
-
-            $DriveLetter = get-Driveletter -path $vhd.path
-            $Path_To_Search = join-path ($DriveLetter)($path)
-            Write-Verbose "Searching for Duplicates in $($vhd.path)"
-
-            if (-not(test-path -path $Path_To_Search)) {
-                Write-Warning "$name : Could not find path: $Path_To_Search"
-            }
-
-            ## Find Duplicate Algorithm ##
-            ## Is there a faster comparing algorithm? ##
-            $files = get-childitem -path $Path_To_Search -Recurse | Sort-Object -Property LastWriteTime -Descending
-            foreach ($file in $files) {
-
-                $currentfile = 0
-                try {
-                    ## Get File's hash value, skipping if folder ##
-                    $get_FileHash = Get-FileHash -path $file.fullname
-                    $FileHash = $get_FileHash.hash
-                    Write-Verbose "Obtained $file's hash value."
-                }
-                catch [System.Management.Automation.PropertyNotFoundException] {
-                    Write-Warning "'$file' is not a file... Skipping."
-                    continue
-                }
-
-                ## Compare Current hash to rest of childitem's hash values ##
-                foreach ($cmpFile in $files | where-object {$_.Name -ne $file.Name}) {
-                    
-                    try {
-                        $get_FileHash = get-filehash -path $cmpFile.fullname
-                        $cmpHash = $get_FileHash.hash
-                    }
-                    catch [System.Management.Automation.PropertyNotFoundException] {
-                        Write-Warning "'$cmpfile' is not a file... Skipping."
-                        continue
-                    }   
-
-                    ## Avoid comparing duplicates we've already checked ##
-                    if ($HashArray.Contains($cmpHash)) {
-                        Write-Verbose "Already found $cmpfile's duplicates"
-                        break;
-                    }
-
-                    if ($FileHash -eq $cmpHash) {
-                        Write-Verbose "Duplicate found!"
-                        if ($currentfile -eq 0) {
-                            $currentfile++
-                            $output = $name + ',' + $file.name + ',' + $cmpFile.fullname
-                        }
-                        else {
-                            $output = ',' + ',' + $cmpFile.FullName
-                        }
-                        Add-Content -path $Csvpath $output
-                    }
-                }
-
-                ## We found all duplicates of this hash. No more comparisons ##
-                $HashArray.Add($FileHash) > $null
-            }
-            dismount-FslDisk -path $vhd.path
+            
+            ## Get-Duplicate Helper function
+            get-FslDuplicateFiles -path $vhd.path -folderpath $Path -csvpath $Csvpath
         }
     }
     
