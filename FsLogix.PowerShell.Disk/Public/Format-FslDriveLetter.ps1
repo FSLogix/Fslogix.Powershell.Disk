972cc2a5f7024f3eb1b69da237a7344584958a3c
Yong Kim <yhk18349@uga.edu>
2018-07-02 13:30:01 -0400

    Format Driveletter function
    Using private helper functions, can get, set, or remove a disk's driveletter.

---
+++ FsLogix.PowerShell.Disk/Public/Format-FslDriveLetter.ps1
@@ -0,0 +1,71 @@
+function Format-FslDriveLetter {
+    [CmdletBinding()]
+    param (
+        
+        [Parameter(Position = 0, Mandatory = $true, 
+        ValueFromPipeline = $true)]
+        [System.String]$VhdPath,
+        
+        [Parameter(Position = 1, Mandatory = $true, 
+        ValueFromPipeline = $true)]
+        [ValidateSet('get','set','remove')]
+        [System.String]$Command,
+
+        [Parameter(Position = 2, Mandatory = $false, 
+        ValueFromPipeline = $true)]
+        [ValidatePattern('^[a-zA-Z]')]
+        [System.Char]$Letter
+
+
+    )
+    
+    begin {
+        set-strictmode -Version latest
+        $GetDL = $false
+        $SetDL = $false
+        $RemoveDL = $false
+    }
+    
+    process {
+        ## Helper function to retrieve VHD's. Will handle errors ##
+        $VHDs = get-fsldisk -Path $VhdPath
+        
+        switch ($Command) {
+            'get' {
+                $GetDL = $true
+            }
+            'set' {
+                $SetDL = $true
+                if($null -eq $Letter){
+                    Write-Warning "Please enter a Drive Letter parameter. Example: Format-FslDriveLetter -Letter G"
+                    Write-Warning "Failed to change Drive Letter."
+                }
+            }
+            'remove' {
+                $RemoveDL = $true
+            }
+        }
+
+        ## Helper functions, Get-DriveLetter, Set-FslDriveletters, remove-fslDriveletter, and dismount-fsldisk
+        ## Will validate error handling.
+
+        foreach ($vhd in $VHDs) {
+            $name = split-path -Path $vhd.path
+            Write-Verbose "Processing VHD: $name"
+            if ($GetDL) {
+                get-driveletter -VHDPath $vhd.path
+                dismount-FslDisk -path $vhd.path
+            }
+            if ($SetDL) {
+                Set-FslDriveLetter -VHDPath $vhd.path -Letter $letter
+            }
+            if ($RemoveDL) {
+                Remove-FslDriveLetter -Path $vhd.path
+            }
+            Write-Verbose "Finished Processing VHD: $name"
+        }
+    }
+    
+    end {
+    }
+}
