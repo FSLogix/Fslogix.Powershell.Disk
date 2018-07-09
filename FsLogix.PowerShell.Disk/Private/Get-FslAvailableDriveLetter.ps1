f5c9bb19b11d72b75405fe040c978031f304d53c
Yong Kim <yhk18349@uga.edu>
2018-07-09 13:10:16 -0400

    Update Get-FslAvailableDriveLetter.ps1

--- FsLogix.PowerShell.Disk/Private/Get-FslAvailableDriveLetter.ps1
+++ FsLogix.PowerShell.Disk/Private/Get-FslAvailableDriveLetter.ps1
@@ -1,9 +1,9 @@
 function Get-FslAvailableDriveLetter{
 
+    ## Start at D rather than A since A-B are floppy drives and C is used by main operating system.
     $Letters = [char]'D'..[Char]'Z' | ForEach-Object { "$([char]$_)" } 
-        
-    $GetLetters = $Letters | where-Object { 'h:', 'k:', 'z:' -notcontains $_  } 
-        
+
+    ## This finds all available driveletters that are not mapped.
     $AvailableLetters = $GetLetters | Where-Object { (new-object System.IO.DriveInfo $_).DriveType -eq 'noRootdirectory' }
  
     Write-Output $AvailableLetters
