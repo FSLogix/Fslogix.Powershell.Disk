893e8a7ffc854adb8b41e14984c1d441310bc392
Yong Kim <yhk18349@uga.edu>
2018-07-11 15:44:27 -0400

    Updated to work for latest version

---
+++ FsLogix.PowerShell.Disk/Public/Export-FslCsv.ps1
@@ -0,0 +1,74 @@
+function Export-FslCsv {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String]$CsvLocation,
+
+        [Parameter(Position = 1)]
+        [System.String]$Destination
+    )
+
+    begin {
+        set-strictmode -Version latest
+    }
+
+    process {
+
+        if ($Destination) {
+            if (-not(test-path -path $Destination)) {
+                Write-Error "Could not find directory: $Destination" -ErrorAction stop
+            }
+            if ((Get-Item $Destination) -isnot [System.IO.DirectoryInfo]) {
+                Write-Error "Destination: $Destination must be a directory, not a file." -ErrorAction Stop
+            }
+        }
+
+        if (test-path -path $CsvLocation) {
+            $CSVFiles = Get-ChildItem -path $CsvLocation -recurse -Filter "*.csv"
+        }
+        else {
+            Write-Error "Could not find directory $CsvLocation" -ErrorAction Stop
+        }
+
+        if ($null -eq $CSVFiles) {
+            Write-Warning "Could not find any CSV files in $CsvLocation"
+        }
+
+        foreach ($csv in $CSVFiles) {
+            $excel = New-Object -ComObject excel.application
+            $workbook = $excel.Workbooks.Add(1)
+            $worksheet = $workbook.worksheets.Item(1)
+
+            ## Get-Delimiter helper function
+            $delimiter = Get-FslDelimiter -csv $csv.fullname
+
+            $TxtConnector = ("TEXT;" + $csv.fullname)
+            $Connector = $worksheet.QueryTables.add($TxtConnector, $worksheet.Range("A1"))
+
+            $query = $worksheet.QueryTables.item($Connector.name)
+            $query.TextFileOtherDelimiter = "$delimiter"
+            $query.TextFileParseType = 1
+            $query.TextFileColumnDataTypes = , 1 * $worksheet.Cells.Columns.Count
+            $query.AdjustColumnWidth = 1
+            $query.Refresh() | Out-Null
+            $query.Delete()
+
+
+            if ($Destination -ne "") {
+                $ExcelDestination = $Destination + "\" + [System.String]$csv.basename + ".xlsx"
+            }
+            else {
+                $ExcelDestination = [System.String]$csv.directory + "\" + [System.String]$csv.basename + ".xlsx"
+            }
+            if (Test-Path -Path $ExcelDestination) {
+                remove-item $ExcelDestination -Force
+            }
+
+            $Excel.ActiveWorkbook.SaveAs($ExcelDestination, 51)
+            Write-Verbose "Sucessfully converted $($csv.name) to Excel format."
+            $Excel.Quit()
+        }
+    }
+    end {
+    }
+}
