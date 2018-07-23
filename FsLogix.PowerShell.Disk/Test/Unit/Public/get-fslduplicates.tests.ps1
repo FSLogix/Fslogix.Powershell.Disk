3c51b5a54f62ab4e6f844ea7b88642ebc6e93c42
Yong Kim <yhk18349@uga.edu>
2018-07-23 16:18:22 -0400

    100% pester Duplicates

--- FsLogix.PowerShell.Disk/Test/Unit/Public/get-fslduplicates.tests.ps1
+++ FsLogix.PowerShell.Disk/Test/Unit/Public/get-fslduplicates.tests.ps1
@@ -6,7 +6,7 @@ $here = $here | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
 
 Describe $sut{
     Context -name "Should throw"{
-        Mock -CommandName Get-FslDuplicateFiles -MockWith {} -Verifiable
+        Mock -CommandName Get-FslDuplicateFiles -MockWith {$true}
         it 'Invalid vhd path'{
             $invalid = {get-fslduplicates -vhdpath "avasdf" -Csvpath "C:\Users\danie\test.csv"}
             $invalid | should throw
@@ -15,15 +15,26 @@ Describe $sut{
             $invalid = {get-FslDuplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\testvhd1.vhdx" -Path 'C:\Users\danie\Documents\VHDModuleProject\FsLogix.PowerShell.Disk\Test\Unit\Public\ConvertTo-fsldisk.tests.ps1' -Csvpath "C:\blah"}
             $invalid | should throw
         }
-        it 'No Vhds in given path'{
-            {get-fslduplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\FsLogix.PowerShell.Disk\Test\Unit\Public"} | Out-Null | should throw
-        }
     }
     Context -name "Should not throw"{
+        BeforeEach{
+            mock -CommandName Get-FslVhd -MockWith{
+                [PSCustomObject]@{
+                    Name = 'Test.vhd'
+                    Path = 'C:\hi'
+                }
+            }
+            mock -CommandName Get-FslDuplicateFiles -MockWith {$true}
+        }
 
         it "Valid path/csv path"{
-            $cmd = {get-fslduplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\testvhd1.vhdx" -csvpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\test.csv"}
-            $cmd | should not throw
+            {get-fslduplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\testvhd1.vhdx" -csvpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\test.csv"} | should not throw
+        }
+        it 'Remove'{
+            {get-fslduplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\testvhd1.vhdx" -csvpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\test.csv" -remove} | should not throw
+        }
+        it 'Index'{
+            {get-fslduplicates -vhdpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\testvhd1.vhdx" -csvpath "C:\Users\danie\Documents\VHDModuleProject\ODFCTest\test.csv" -Remove -Start 1 -End 2} | should not throw
         }
     }
 }
