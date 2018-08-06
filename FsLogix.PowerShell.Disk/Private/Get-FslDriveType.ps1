c6ea2b9f6b129d6fb8fb13eceb7ea8c08a7426de
Daniel0226 <batehampandora@gmail.com>
2018-08-06 12:16:05 -0400

    SO MUCH FASTER!!

--- FsLogix.PowerShell.Disk/Private/Get-FslDriveType.ps1
+++ FsLogix.PowerShell.Disk/Private/Get-FslDriveType.ps1
@@ -15,11 +15,9 @@ function Get-FslDriveType {
     process {
         
         Write-Verbose "$((get-date).ToString('yy/mm/dd/hh:mm:ss:fff'))"
-        $Disk = get-disk -Number $DiskNumber
-
         # This line of code is really slow
         # Need to improve runtime of this command.
-        $Partition_AccessPaths = ($Disk | Get-Partition).AccessPaths | select-object -first 1
+        $Partition_AccessPaths = (Get-Partition -DiskNumber 1).AccessPaths | select-object -first 1
         Write-Verbose "$((get-date).ToString('yy/mm/dd/hh:mm:ss:fff'))"
         
         $volume = Get-WMIObject -Class Win32_Volume | Where-Object {$_.DeviceId -eq $Partition_AccessPaths}
