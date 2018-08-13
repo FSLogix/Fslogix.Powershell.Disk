7481b953985b5771e4d1931f7c7e243f185a8dab
Daniel0226 <batehampandora@gmail.com>
2018-08-13 10:23:27 -0400

    Fixed Guid bug

--- FsLogix.PowerShell.Disk/Public/Clear-FslGuid.ps1
+++ FsLogix.PowerShell.Disk/Public/Clear-FslGuid.ps1
@@ -2,11 +2,7 @@ function Clear-FslGuid {
     [CmdletBinding()]
     param (
         [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateScript({
-            if(-not(test-path -path $_)){
-                Throw "Could not find Guid path: $_"
-            }
-        })][System.String]$GuidPath       
+        [System.String]$GuidPath       
     )
     
     begin {
@@ -15,17 +11,24 @@ function Clear-FslGuid {
     
     process {
         if(!$GuidPath){
-            $GuidPath = 'C:\programdata\FsLogix\FslGuid'
+            $GuidPath = "C:\programdata\FsLogix\FslGuid"
+        }
+        if(-not(test-path $GuidPath)){
+            Write-Error "Could not found path: $GuidPath" -ErrorAction Stop
         }
       
 
-        $VHD_Guid_path = get-childitem -path $GuidPath
+        $VHD_Guid_path = (get-childitem -path $GuidPath).FullName
         if($null -eq $VHD_Guid_path){
             Write-Warning -Message "Guid is already cleared"
             exit
         }
         foreach($path in $VHD_Guid_path){
-            Remove-item -Path $Path -Force -ErrorAction SilentlyContinue
+            try{
+                Remove-item -Path $Path -Force -ErrorAction SilentlyContinue
+            }catch{
+                Write-Error "Could not remove guid: $path"
+            }
         }
         Write-Verbose "Cleared all Guid paths in $GuidPath."
     }
--- FsLogix.PowerShell.Disk/Test/Unit/Public/Clear-FslGuid.tests.ps1
+++ FsLogix.PowerShell.Disk/Test/Unit/Public/Clear-FslGuid.tests.ps1
@@ -5,16 +5,27 @@ $here = $here | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
 . "$here\$funcType\$sut"
 
 Describe $sut {
-    BeforeAll{
-        mock -CommandName remove-item -MockWith {} -Verifiable
+   
+    Context -Name 'General tests' {
+        BeforeEach {
+            mock -CommandName remove-item -MockWith {} -Verifiable
+        }
+        it 'should throw' {
+            {Clear-FslGuid -GuidPath 'C:\blah'} | should throw
+        }
+        it 'should not throw' {
+            {Clear-FslGuid} | should not throw
+        }
+        it 'should not throw' {
+            {Clear-FslGuid -GuidPath 'C:\programdata\FsLogix\FslGuid'} | should not throw
+        }
     }
-    it 'should throw'{
-        {Clear-FslGuid -GuidPath 'C:\blah'} | should throw
+    context -name 'Test folder' {
+        it 'Should be empty' {
+            Clear-FslGuid
+            $File_Contents = get-childitem 'C:\programdata\FsLogix\FslGuid'
+            $File_Contents | should be $null
+        }
     }
-    it 'should not throw'{
-        {Clear-FslGuid} | should not throw
-    }
-    it 'should not throw'{
-        {Clear-FslGuid -GuidPath 'C:\programdata\FsLogix\FslGuid'} | should not throw
-    }
+    
 }
