bf9cc577c4bc32a65ee7b3bbc7ea2cfac573bd5f
Daniel0226 <batehampandora@gmail.com>
2018-08-15 10:25:19 -0400

    New test outputs

--- FsLogix.PowerShell.Disk/Test/Unit/Public/Get-FslDiskInfo.tests.ps1
+++ FsLogix.PowerShell.Disk/Test/Unit/Public/Get-FslDiskInfo.tests.ps1
@@ -35,13 +35,34 @@ Describe $sut{
         it 'disk info csv'{
             {Get-fsldiskinfo -Csvfile 'C:\Users\danie\Documents\VHDModuleProject\test.csv'} | should not throw
         }
-        it 'test output'{
+        it 'test output1'{
             $output = get-fsldiskinfo -path $VHD
             $output.name | should be $(Split-Path -path $VHD -leaf)
             $output.location | should be $vhd
         }
+        it 'test output 2'{
+            $output = Get-FslDiskInfo -path $VHD
+            $output.format | should be 'VHD'
+            $output.Type | should be 'Dynamic'
+        }
+        it 'test output 3'{
+            mock -CommandName get-fslvhd -MockWith {
+                [PSCustomObject]@{
+                    ComputerName  = $Env:COMPUTERNAME
+                    Path          = 'C:\Users\danie\Documents\VHDModuleProject\ODFCTest\NoDl.vhdx'
+                    location      = 'C:\Users\danie\Documents\VHDModuleProject\ODFCTest\NoDl.vhdx'
+                    vhdFormat        = 'VHDx'
+                    vhdType          = 'Fixed'
+                    Size          = 1gb
+                    Filesize      = .5gb
+                 }
+            }-Verifiable
+            $output = Get-FslDiskInfo -path 'C:\Users\danie\Documents\VHDModuleProject\ODFCTest\NoDl.vhdx'
+            $output.location | should be 'C:\Users\danie\Documents\VHDModuleProject\ODFCTest\NoDl.vhdx'
+            $output.Format | should be 'VHDx'
+        }
         it 'Asserts all verifiable mocks' {
-            Assert-VerifiableMocks
+            Assert-VerifiableMock
         }
     }
 }
