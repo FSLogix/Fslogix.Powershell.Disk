13900cedff27952e6604317b5ce9ca68c3f30266
Daniel0226 <batehampandora@gmail.com>
2018-10-04 12:57:20 -0400

    path fix

--- FsLogix.Powershell.Disk/loadfunctions.ps1
+++ FsLogix.Powershell.Disk/loadfunctions.ps1
@@ -1,5 +1,5 @@
-$Public = @( Get-ChildItem -Path 'C:\Users\danie\Documents\Scripts\Disk\Fslogix.Powershell.Disk\DiskModule\Functions\Public' -filter "*.ps1")
-$Private = @( Get-ChildItem -Path 'C:\Users\danie\Documents\Scripts\Disk\Fslogix.Powershell.Disk\DiskModule\Functions\Private' -filter "*.ps1")
+$Public = @( Get-ChildItem -Path 'C:\Users\danie\Documents\Scripts\Disk\Fslogix.Powershell.Disk\FsLogix.PowerShell.disk\Functions\Public' -filter "*.ps1")
+$Private = @( Get-ChildItem -Path 'C:\Users\danie\Documents\Scripts\Disk\Fslogix.Powershell.Disk\FsLogix.PowerShell.disk\Functions\Private' -filter "*.ps1")
 $verbosepreference = "continue"
 #Dot source the filesd
 Foreach ($import in @($Public + $Private)) {
