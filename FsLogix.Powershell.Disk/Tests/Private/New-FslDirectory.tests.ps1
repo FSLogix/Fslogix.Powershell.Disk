f9bed13749b5fb4c7e7ba8197d69f173f14e590e
Daniel0226 <batehampandora@gmail.com>
2018-10-09 12:13:49 -0400

    Pester

---
+++ FsLogix.Powershell.Disk/Tests/Private/New-FslDirectory.tests.ps1
@@ -0,0 +1,78 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$here = $here -replace 'Tests', 'Functions'
+
+$SamAccountName = "Daniel"
+$SID = "0-2-26-1996"
+$Destination = "test"
+$Destination2 = "test\%Username%"
+
+Describe $sut{
+
+    BeforeAll{
+        Mock -CommandName Remove-item -MockWith {}
+        Mock -CommandName New-Item -MockWith {}
+        #mock -CommandName Test-path -MockWith {}
+    }
+    Context -Name "Input"{
+        it 'takes normal input'{
+            {New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination} | should not throw
+        }
+        it 'accepts pipeline input'{
+            {$SamAccountName | New-FslDirectory -SID $SID -Destination $Destination } | should not throw
+        }
+        it 'accepts pipeline input2'{
+            {$SID | New-FslDirectory -SamAccountName $SamAccountName -Destination $Destination } | should not throw
+        }
+        it 'Positional arguments'{
+            {New-FslDirectory $SamAccountName $SID $Destination} | should not throw
+        }
+        it '%username%'{
+            {New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination2} | should not throw
+        }
+    }
+    Context -name 'Test-path'{
+        Mock -CommandName test-path -MockWith {$True}
+        it 'Remove directory'{
+            {New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination} | should not throw
+            Assert-MockCalled -CommandName Remove-item -Times 1
+        }
+    }
+    Context -name 'Flip-Flop'{
+        it "Normal"{
+            $command = New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination -Passthru
+            $command | should be "test\Daniel_0-2-26-1996"
+        }
+
+        it "Flip-Flop"{
+            $command = New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination -Passthru -FlipFlop
+            $command | should be "test\0-2-26-1996_Daniel"
+        }
+    }
+    Context -name 'Passthru'{
+        it 'no output'{
+            $command = New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination
+            $command | should be $null
+        }
+        it 'passthru output'{
+            $command = New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination -Passthru
+            $command | should be "test\Daniel_0-2-26-1996"
+        }
+        it '%username%'{
+            $command = New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination2 -Passthru
+            $command | should be "test\Daniel_0-2-26-1996"
+        }
+    }
+    Context -name 'New-Item'{
+        Mock -CommandName New-Item -MockWith {
+            Throw 'New'
+        }
+        it 'Throws'{
+            {New-FslDirectory -SamAccountName $SamAccountName -SID $SID -Destination $Destination -ErrorAction Stop} | should throw
+        }
+        it 'assert mock called'{
+            Assert-MockCalled -CommandName New-Item -Times 1
+        }
+
+    }
+}
