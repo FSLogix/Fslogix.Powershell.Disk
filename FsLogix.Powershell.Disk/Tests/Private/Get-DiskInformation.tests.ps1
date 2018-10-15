cb2d64a0501185f54ac83d7dd7606ea66f422ab3
Daniel0226 <batehampandora@gmail.com>
2018-10-15 10:32:18 -0400

    more tests

--- FsLogix.Powershell.Disk/Tests/Private/Get-DiskInformation.tests.ps1
+++ FsLogix.Powershell.Disk/Tests/Private/Get-DiskInformation.tests.ps1
@@ -3,6 +3,48 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $here = $here -replace 'Tests', 'Functions'
 . "$here\$sut"
 
-Describe $sut{
+$Path = "C:\Users\danie\Documents\VHDModuleProject\ODFCtest3\FsLTest.vhdx"
 
+Describe $sut{
+    BeforeAll{
+        Mock -CommandName Get-Diskimage -MockWith {
+            [PSCustomObject]@{
+                Size = 1000
+            }
+        }
+        Mock -CommandName Get-Item -MockWith {
+            [PSCustomObject]@{
+                Extension = ".vhdx"
+                BaseName = "FslTest"
+            }
+        }
+    }
+    Context -name "Outputs"{
+        it 'Test outputs'{
+            $output = Get-diskinformation -path $Path
+            $output.Name | should be "FslTest.vhdx"
+            $output.BaseName | should be "FslTest"
+            $output.Format | should be "vhdx"
+        }
+        Mock -CommandName Get-Diskimage -MockWith {
+            Throw 'Image'
+        }
+        it 'Throws'{
+            {Get-Diskinformation $Path -ErrorAction Stop} | should throw
+        }
+        it 'assert mock called'{
+            Assert-MockCalled -CommandName Get-Diskimage -Times 1
+        }
+    }
+    Context -name "Inputs"{
+        it 'Accepts normal input'{
+            {Get-DIskinformation -path $Path} | should not throw
+        }
+        it 'Accepts pipeline'{
+            {$Path | Get-Diskinformation} | should not throw
+        }
+        it 'Positional'{
+            {Get-Diskinformation $path} | should not throw
+        }
+    }
 }
