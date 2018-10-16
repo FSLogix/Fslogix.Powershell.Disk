dd8b29f29140a2f26d7e4fc310cb769f85c2baf4
Daniel0226 <batehampandora@gmail.com>
2018-10-16 10:30:06 -0400

    Added Resize

--- FsLogix.Powershell.Disk/Functions/Public/Set-FslDisk.ps1
+++ FsLogix.Powershell.Disk/Functions/Public/Set-FslDisk.ps1
@@ -1,40 +1,36 @@
 function Set-FslDisk {
+    <#
+        .PARAMETER SIZE
+        New size of the VHD. Input is either a number, or xMB, xGB, xTB.
+    #>
     [CmdletBinding(DefaultParameterSetName = "None")]
     param (
         [Parameter( Position = 0,
-                    Mandatory = $true,
-                    ValueFromPipeline = $true,
-                    ValueFromPipelineByPropertyName = $true)]
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
         [System.String]$Path,
 
         [Parameter (Position = 1,
-                    Mandatory = $true,
-                    ParameterSetName = "Label")]
+            Mandatory = $true,
+            ParameterSetName = "Label")]
         [System.String]$Label,
 
         [Parameter (Position = 2,
-                    ParameterSetName = "Label")]
+            ParameterSetName = "Label")]
         [Switch]$Assign,
 
         [Parameter (Position = 1,
-                    Mandatory = $true,
-                    ParameterSetName = "Name")]
+            Mandatory = $true,
+            ParameterSetName = "Name")]
         [System.String]$Name,
 
         [Parameter (Position = 1,
-                    Mandatory = $true,
-                    ParameterSetName = "Size")]
+            Mandatory = $true,
+            ParameterSetName = "Size")]
         [uint64]$size,
 
-        [Parameter (Position = 1,
-                    Mandatory = $true,
-                    ParameterSetName = "vhdx")]
-        [Switch]$vhdx,
-
-        [Parameter (Position = 1,
-                    Mandatory = $true,
-                    ParameterSetName = "Vhd")]
-        [Switch]$vhd
+        [switch]$Dismount
     )
     
     begin {
@@ -44,58 +40,101 @@ function Set-FslDisk {
     
     process {
 
-        if(-not(test-path -path $Path -ErrorAction Stop)){
+        if (-not(test-path -path $Path -ErrorAction Stop)) {
             Write-Error "Could not find path: $Path" -ErrorAction Stop
         }
        
         $VHDinfo = Get-Fsldisk -Path $Path -ErrorAction Stop
     
-        Switch ($PSBoundParameters.Keys){
-            Label{
-                Try{
+        Switch ($PSBoundParameters.Keys) {
+            Label {
+                Try {
                     Set-FslLabel -Path $Path -Label $Label -ErrorAction Stop
-                }catch{
+                }
+                catch {
                     Write-Error $Error[0]
                 }
-                if($Assign){
-                    Try{
+                if ($Assign) {
+                    Try {
                         Add-FslDriveLetter -Path $Path -ErrorAction Stop
-                    }catch{
+                    }
+                    catch {
                         Write-Error $Error[0]
                     }
                 }
             }
-            Name{
+            Name {
                 ## What should the name be. CDW had name of 'ODFC_SamAccountName'
                 ## so is the regex match for SID_Name or Name_SID neccessary?
                 
                 #Can't rename if VHD is attached
-                if($VHDinfo.attached){
-                    Try{
+                if ($VHDinfo.attached) {
+                    Try {
                         Dismount-DiskImage -ImagePath $Path
-                    }catch{
+                    }
+                    catch {
                         Write-Error $Error[0]
                     }
                 }
                 
-                try{
+                try {
                     Rename-Item -Path $Path -NewName $Name -ErrorAction Stop
                     Write-Verbose "Renamed $($VHDinfo.name) to $Name"
                 }
-                catch{
+                catch {
+                    Write-Warning "Could not rename VHD."
                     Write-Error $Error[0]
                 }
             }
             Size {
-                # Resize VHD
-            }
-            
-            VHDx {
-                # Convert to VHDx
-            }
+                #Requires -Modules "Hyper-V"
 
-            VHD {
-                # Convert to VHD
+                Try {
+                    Resize-VHD -Path $Path -SizeBytes $size -ErrorAction Stop
+                    Write-Verbose "Successfully sized VHD: $($VHDinfo.name)."
+                }
+                catch {
+                    Write-Warning "Could not resize VHD."
+                    Write-Error $Error[0]
+                    exit
+                }
+
+                if ($VHDinfo.attached) {
+                    $Disk = Get-Disk | Where-Object {$_.Location -eq $Path}
+                    $DiskNumber = $Disk.number
+                }
+                else {
+                    $Disk = Mount-FslDisk -Path $Path
+                    $DiskNumber = $Disk.DiskNumber
+                }
+
+                
+                Try {
+                    $PartitionSize = Get-PartitionSupportedSize -DiskNumber $DiskNumber -PartitionNumber 1 -ErrorAction Stop | Select-Object -ExpandProperty Sizemax
+                }
+                catch {
+                    Write-Warning "Could not retrieve supported size max."
+                    Write-Error $Error[0]
+                    exit
+                }
+                Try {
+                    Resize-Partition -DiskNumber $DiskNumber -PartitionNumber 1 -Size $PartitionSize -ErrorAction Stop
+                    Write-Verbose "Successfully reformated partition."
+                }
+                catch {
+                    Write-Warning "Could not reformat partition."
+                    Write-Error $Error[0]
+                    exit
+                }
+            }
+        } # Switch
+
+        if ($PSBoundParameters.ContainsKey("Dismount")){
+            Try{
+                Dismount-FslDisk -Path $Path -ErrorAction Stop
+            }catch{
+                Write-Warning "Could not dismount VHD."
+                Write-Error $Error[0]
             }
         }
     }
--- FsLogix.Powershell.Disk/Tests/Public/Set-FslDisk.tests.ps1
+++ FsLogix.Powershell.Disk/Tests/Public/Set-FslDisk.tests.ps1
@@ -5,24 +5,47 @@ $here = $here -replace 'Tests', 'Functions'
 
 Describe $sut{
 
-    $Path = "C:\Users\danie\Documents\VHDModuleProject\ODFCtest3\FsLTest.vhdx"
-
+    $Script:DriveLetter = "D:\"
+    $Script:Path = "C:\Users\danie\Documents\VHDModuleProject\ODFCtest3\FsLTest.vhdx"
+    $Script:TestDisk = [PSCustomObject]@{
+        Attached = $true
+        Name     = "Test"
+    }
+    $Script:TestGetDisk = [PSCustomObject]@{
+        Location = "C:\Users\danie\Documents\VHDModuleProject\ODFCtest3\FsLTest.vhdx"
+        Number = 1
+    }
+    $Script:TestMount = [PSCustomObject]@{
+        DiskNumber = 1
+    }
+    $Script:TestMaxSize = [PSCustomObject]@{
+        SizeMax = 100
+    }
     BeforeAll{
         Mock -CommandName Get-FslDriveletter -MockWith {
-            "D:\"
+            $Script:DriveLetter
         }
         Mock -CommandName Get-FslDisk -MockWith {
-            [PSCustomObject]@{
-                Attached    = $true
-                Name        = "Test"
-            }
+            $Script:TestDisk
         }
+        mock -CommandName Add-FslDriveLetter -MockWith {
+            $Script:DriveLetter
+        }
+        Mock -CommandName Get-Disk -MockWith {
+            $Script:TestGetDisk
+        }
+        Mock -CommandName Mount-FslDisk -MockWith {
+            $Script:TestMount
+        }
+        Mock -CommandName Get-PartitionSupportedSize -MockWith {
+            $Script:TestMaxSize
+        }
+        Mock -CommandName Resize-Vhd -mockwith {}
+        Mock -CommandName Resize-Partition -MockWith {}
         Mock -CommandName Set-Volume -MockWith {}
         Mock -CommandName Dismount-DiskImage -MockWith {}
         Mock -CommandName Rename-item -MockWith {}
-        mock -CommandName Add-FslDriveLetter -MockWith {
-            "D:\"
-        }
+        Mock -CommandName Dismount-fsldisk -MockWith {}
         Mock -CommandName Set-FslLabel -MockWith {}
     }
     Context -Name "General Throws"{
@@ -104,6 +127,109 @@ Describe $sut{
         }
     }
 
+    Context -name "Size input"{
+        it 'Accepts normal input'{
+            {Set-Fsldisk -path $Path -size 1gb} | should not throw
+        }
+        it 'Accepts pipeline input'{
+            {$Path | set-fsldisk -size 1gb} | should not throw
+        }
+    }
+    Context -name "Resize-VHD"{
+        Mock -CommandName Resize-VHD -MockWith {
+            Throw 'VHD'
+        }
+        $Error.clear()
+        it 'Throws'{
+            {Set-Fsldisk -path $Path -size 1gb -ErrorAction Stop} | should throw
+        }
+        it 'Assert mock called'{
+            Assert-MockCalled -CommandName Resize-VHD -Times 1
+        }
+        it 'Error was thrown'{
+            $Error.Count | should be 2
+        }
+        it "assert script stopped"{
+            Assert-MockCalled -CommandName Resize-Partition -Times 0
+        }
+    }
+
+    Context -name "Size variance"{
+        it "Attached should call Get-Disk"{
+            $Script:TestDisk = [PSCustomObject]@{
+                Attached = $true
+                Name     = "Test"
+            }
+            Mock -CommandName Get-FslDisk -MockWith {
+                $Script:TestDisk
+            }
+            {Set-Fsldisk -path $Path -size 1gb} | should not throw
+            Assert-MockCalled -CommandName Get-Disk -Times 1
+        }
+        it "Not attached should call Mount-FslDisk"{
+            $Script:TestDisk = [PSCustomObject]@{
+                Attached = $false
+                Name     = "Test"
+            }
+            Mock -CommandName Get-FslDisk -MockWith {
+                $Script:TestDisk
+            }
+            {Set-Fsldisk -path $Path -size 1gb} | should not throw
+            Assert-MockCalled -CommandName Mount-FslDisk -Times 1
+        }
+    }
+    Context -name "Get-PartitionSupportedSize"{
+        Mock -CommandName Get-PartitionSupportedSize -MockWith {
+            Throw 'Size'
+        }
+        $Error.clear()
+        it 'Throws'{
+            {Set-Fsldisk -path $Path -size 1gb -ErrorAction Stop} | should throw
+        }
+        it 'Assert mock called'{
+            Assert-MockCalled -CommandName Get-PartitionSupportedSize -Times 1
+        }
+        it 'Error was thrown'{
+            $Error.Count | should be 2
+        }
+        it "assert script stopped"{
+            Assert-MockCalled -CommandName Resize-Partition -Times 0
+        }
+    }
+    Context -name "Resize-Partition"{
+        Mock -CommandName Resize-Partition -MockWith {
+            Throw 'resize'
+        }
+        $Error.clear()
+        it 'Throws'{
+            {Set-Fsldisk -path $Path -size 1gb -Dismount -ErrorAction Stop} | should throw
+        }
+        it 'Assert mock called'{
+            Assert-MockCalled -CommandName Resize-Partition -Times 1
+        }
+        it 'Error was thrown'{
+            $Error.Count | should be 2
+        }
+        it "assert script stopped"{
+            Assert-MockCalled -CommandName Dismount-fsldisk -Times 0
+        }
+    }
+    Context -name "Dismount"{
+        it 'No dismount switch should not call dismount'{
+            {Set-Fsldisk -path $Path -size 1gb } | should not throw
+            Assert-MockCalled -CommandName Dismount-fsldisk -Times 0
+        }
+        it 'Calls dismount'{
+            {Set-Fsldisk -path $Path -size 1gb -Dismount} | should not throw
+            Assert-MockCalled -CommandName Dismount-fsldisk -Times 1
+        }
+        it 'Throws'{
+            Mock -CommandName Dismount-fsldisk -MockWith {
+                Throw 'Dismount'
+            }
+            {Set-Fsldisk -path $Path -size 1gb -Dismount -ErrorAction Stop} | should throw
+        }
+    }
     Context -Name "Future Tests"{
         
     }
