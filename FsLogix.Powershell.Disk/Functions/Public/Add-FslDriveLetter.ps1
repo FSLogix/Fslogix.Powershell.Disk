9e5cb59944b3c7fe5527258e38f60994019cbf3a
Daniel0226 <batehampandora@gmail.com>
2018-10-29 13:55:07 -0400

    Added description

--- FsLogix.Powershell.Disk/Functions/Public/Add-FslDriveLetter.ps1
+++ FsLogix.Powershell.Disk/Functions/Public/Add-FslDriveLetter.ps1
@@ -1,4 +1,41 @@
 function Add-FslDriveLetter {
+    <#
+        .SYNOPSIS
+        Assigns the next available driveletter starting with Z.
+
+        .DESCRIPTION
+        Starting from letter Z, script validates if drive letter is available and assigns
+        the partition. If Z is not available, the script will check the next available letter, 'Y',
+        and will continue to until completion. If a drive letter is not available then the partition
+        will be not set.
+
+        .PARAMETER Path
+        Mandatory variable, path to the virtual disk. This path should include the virtual disk with the .vhd/.vhdx extension.
+        
+        .PARAMETER PartitionNumber
+        Non-Mandatory variable, determines specific partitionnumber. FsLogix virtual disk's main partition is assigned
+        to partitionnumber 1. If no partitionnumber is assigned, the script will default to 1.
+
+        .PARAMETER Dismount
+        Switch parameter is user wants the virtual disk dismounted upon completion, otherwise the virtual disk
+        will remain mounted.
+
+        .PARAMETER PassThru
+        Switch Parameter to output the drive letter that was assigned to the virtual disk upon completion.
+        Output will follow the format of <C:\>
+
+        .EXAMPLE
+        Add-FslDriveLetter -path 'C:\test\test.vhdx'
+        Script will assign the next available driveletter to virtual disk: test.vhdx
+
+        .EXAMPLE
+        Add-FslDriveLetter 'C:\test\test.vhdx'
+        Script will accept positional path and assign the next available driveletter.
+
+        .EXAMPLE
+        Add-FslDriveLetter 'C:\test\test2.vhdx' -PartitionNumber 2
+        Script will assign 
+    #>
     [CmdletBinding()]
     param (
         [Parameter( Position = 0,
